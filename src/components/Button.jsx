c26717bd00cf323c27854dc3ae542657747f25ec
punyeeet <111042058+punyeeet@users.noreply.github.com>
2023-08-07 22:42:48 +0530

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({style}) => {
    return (
-    <a href='https://youtube.com/shorts/xHEgHjJvR94?feature=share' target="_blank">
+    <a href='#' target="_blank">
     <button type='button' className={` py-4 px-12 bg-blue-gradient ${style} rounded-lg font-medium hover:text-slate-600 `}>
       Get Started
     </button>
